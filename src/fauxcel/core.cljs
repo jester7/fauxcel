26abc7a24a3f4ce3a92ae817d0e7c395749639f0
Jovan Jester <jovan@jester.cafe>
2023-10-19 02:56:40 -0600

    added clippy for fun and disabled toolbar buttons

--- src/fauxcel/core.cljs
+++ src/fauxcel/core.cljs
@@ -1,8 +1,10 @@
 (ns fauxcel.core
   (:require
+   [reagent.core :as r]
    [reagent.dom :as d]
    [fauxcel.components.toolbar.toolbar :refer [toolbar]]
-   [fauxcel.components.spreadsheet :refer [cellgrid]]))
+   [fauxcel.components.spreadsheet :refer [cellgrid]]
+   [fauxcel.util.dom :as dom]))
 
 ;; -------------------------
 ;; Views
@@ -10,6 +12,7 @@
 (defn home-page []
   [:div.wrapper
    [toolbar]
+   [:div#clippy]
    [cellgrid]])
 
 ;; -------------------------
@@ -19,4 +22,5 @@
   (d/render [home-page] (.getElementById js/document "app")))
 
 (defn ^:export init! []
+  (r/after-render #(dom/draggable (dom/querySelector "#clippy") (dom/querySelector "html")))
   (mount-root))
