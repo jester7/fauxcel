0342402fc0e5631dcea768ea184377c6268d247c
Jovan Jester <jovan@jester.cafe>
2023-10-20 00:04:26 -0600

    moved cell range RegExs here and fixed bugs in them

--- src/fauxcel/base/constants.cljs
+++ src/fauxcel/base/constants.cljs
@@ -2,5 +2,10 @@
 
 (def ^:const max-cols 27)
 (def ^:const max-rows 101)
+(def ^:const cell-range-re #"([a-zA-Z]{1,2}[0-9]{0,4})[\:]([a-zA-Z]{1,2}[0-9]{0,4})")
+(def ^:const cell-range-check-re #"^[a-zA-Z]{1,2}[0-9]{0,4}[\:][a-zA-Z]{1,2}[0-9]{0,4}$")
+(def ^:const cell-range-start-end-re #"^([a-zA-Z]{1,2}[0-9]{0,4})[\:]([a-zA-Z]{1,2}[0-9]{0,4})$")
+
+(def ^:const cell-ref-re #"^([a-zA-Z]{1,2})([0-9]{0,4})")
 
 (def ^:const empty-cell {:formula "" :format "" :value ""})
