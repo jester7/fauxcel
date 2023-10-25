baeff7820a23ec7818aa72789a2a85ac501a4909
Jovan Jester <jovan@jester.cafe>
2023-10-24 20:48:27 -0600

    fixed on-click when in edit-mode

--- src/fauxcel/components/spreadsheet.cljs
+++ src/fauxcel/components/spreadsheet.cljs
@@ -3,7 +3,9 @@
    [fauxcel.base.state :as state :refer [cells-map edit-mode current-selection]]
    [fauxcel.base.constants :as constants :refer [max-cols max-rows]]
    [fauxcel.base.parser :as parser]
-   [fauxcel.base.utility :as base-util :refer [update-selection! col-label selection-cell-ref recursive-deref]]))
+   [fauxcel.base.utility :as base-util :refer [update-selection! col-label
+                                               selection-cell-ref recursive-deref
+                                               cell-ref-for-input]]))
 
 (defn cellgrid []
   [:div.cellgrid.wrapper (base-util/keyboard-navigation)
@@ -23,13 +25,13 @@
                                  #(base-util/changed! (.-target %1))
                                  :on-click
                                  (fn [e]
-                                   ;(reset! edit-mode false)
-                                   (when (not (nil? (selection-cell-ref)))
-                                     (reset! edit-mode false)
-                                     (set! (-> (selection-cell-ref) .-value)
-                                           (recursive-deref (:value (@cells-map @current-selection) )))
-                                     (set! (-> (selection-cell-ref) .-readOnly) true))
-                                   (update-selection! (.-target e)))
+                                   (when (and @edit-mode (not= @current-selection (cell-ref-for-input (.-target e))))
+                                     (when (not (nil? (selection-cell-ref)))
+                                       (reset! edit-mode false)
+                                       (set! (-> (selection-cell-ref) .-value)
+                                             (recursive-deref (:value (@cells-map @current-selection))))
+                                       (set! (-> (selection-cell-ref) .-readOnly) true))
+                                     (update-selection! (.-target e))))
                                  :on-double-click
                                  (fn [e]
                                    (reset! edit-mode true)
