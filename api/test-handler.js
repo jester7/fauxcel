f2b0310ba0106bf4365bfce674f140dd7bec8e13
Jovan Jester <jovan@jester.cafe>
2023-10-29 16:03:37 -0600

    test 4

--- api/test-handler.js
+++ api/test-handler.js
@@ -1,18 +1,9 @@
-var shadow$provide = {};
-(function(root, factory) {
-  if (typeof define === "function" && define.amd) {
-    define([], factory);
-  } else if (typeof module === "object" && module.exports) {
-    module.exports = factory();
-  } else {
-    root.returnExports = factory();
-  }
-})(this, function() {
-  var shadow$umd$export = null;
-'use strict';/*
+export const $APP = {};
+export const shadow$provide = {};
+export const $jscomp = {};
+/*
 
  Copyright The Closure Library Authors.
  SPDX-License-Identifier: Apache-2.0
 */
-shadow$umd$export={default$:function(b,a){return a.send("hello world!")}};return shadow$umd$export;
-});
+export default function(b,a){return a.send("hello world!")};
